c382a6ba40b3c193401d324e68130b1332343bdc
Thomas Ashish Cherian <reach2ashish@users.noreply.github.com>
2016-12-18 01:43:55 +0530

    Update bulk_rss_extract.py

--- bulk_rss_extract.py
+++ bulk_rss_extract.py
@@ -16,4 +16,37 @@ def get_rss_feed(website_url):
             """
             Function to extract the feed URL
             """
-  
+def get_urls(inputfile):
+    urls_file = open(inputfile)
+    urls = []
+    g=''
+    for lines in url_file:
+        g=g+'\n'
+    GRUBER_URLINTEXT_PAT = re.compile(r'(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?\xab\xbb\u201c\u201d\u2018\u2019]))')
+    urls.append(re.findall(GRUBER_URLINTEXT_PAT, g)[0][0])
+    urls_file.close()
+    return urls
+def get_bulk_links(urls):
+    g=""
+    for url in urls:
+        myrss=get_rss_feed(url)
+        if len(myrss)==0:
+            g+=url+" has no RSS links\n"
+        else:
+            for links in myrss:
+                g+=url+" ---> " +links
+    return g
+
+
+#The main function 
+#Enter the input as well as the output file path
+def main():
+    print("Enter the input file link")
+    inf=input()
+    print("Enter the output file link")
+    otf=input()
+    g=get_bulk_links(get_urls(inf))
+    otfile=open(otf,'r+')
+    otfile.write(g)
+    otfile.close()
+    
